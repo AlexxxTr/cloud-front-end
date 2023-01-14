1487e05247cd2354a9ce76ffa2908eb0fc0e33e7
Alexander Truyaert <alexander.truyaert@outlook.com>
2023-01-14 14:43:30 +0100

    AT: Updated supabase to have the types inserted

--- src/lib/supabase/createClient.ts
+++ src/lib/supabase/createClient.ts
@@ -1,7 +1,8 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "../types/types";
 
 const supabaseUrl =
   import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_URL ?? "http://localhost:3000";
 const supabaseKey = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
