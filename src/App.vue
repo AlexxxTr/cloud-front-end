1e1aa7c56e0a72a9cc8e4c3ea16f52577088884f
Alexander Truyaert <alexander.truyaert@outlook.com>
2023-01-14 14:36:32 +0100

    AT: Added page where we will let the user add his gas usage data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { supabase } from "./lib/supabase/createClient";
 import { RouterLink, RouterView } from "vue-router";
-
 let isLoggedIn = false;
-supabase.auth.onAuthStateChange(loggedIn);
+loggedIn();
 
 async function loggedIn() {
   const user = await supabase.auth.getUser();
@@ -26,6 +25,16 @@ loggedIn();
         </RouterLink>
         <RouterLink to="/home" class="relative group" v-if="!isLoggedIn">
           <span>Home</span>
+          <span
+            class="absolute -bottom-1 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"
+          />
+        </RouterLink>
+        <RouterLink
+          to="/add-gas-usage"
+          class="relative group"
+          v-if="!isLoggedIn"
+        >
+          <span>Add Gas Usage</span>
           <span
             class="absolute -bottom-1 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"
           />
---
+++ src/views/AddGasView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>Some Form will Come here</main>
+</template>
